081258ff6197a68024ad6ec4b0e4d9d6b9b2e585
datHero <datsomorg@gmail.com>
2019-08-07 20:34:08 +0000

    Add a unit test for ghanaian week name

---
+++ test.py
@@ -0,0 +1,21 @@
+from unittest import mock, TestCase, main
+
+from app import GhanianName
+
+
+class NameTestCase(TestCase):
+
+    def test_weekday_name(self):
+        app = GhanianName()
+        app.gender = 'm'
+        app.dob = '14/11/1972'
+        self.assertEqual(app.get_weekday_name(), 'Tuesday')
+    
+    def test_dob_weekday_name(self):
+        app=GhanianName()
+        app.gender ='m'
+        app.dob = '13/06/1998'
+        self.assertEqual(app.get_ghanian_name(),'Kwame')
+
+if __name__ == '__main__':
+    main()
